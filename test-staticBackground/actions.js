58a481557e3d00549c967a6561f6531b0e8b6803
Eric López <eric@ela.0.1.1>
2012-09-21 21:45:07 +0200

    Adding testing files (graient background, simple background, textarea with border...)

---
+++ test-staticBackground/actions.js
@@ -0,0 +1,21 @@
+var ambientalSoundUrl = 'song.ogg';
+var ambientalAudio = document.createElement('audio');
+ambientalAudio.src = ambientalSoundUrl;
+
+function playAmbientSound() {
+	ambientalAudio.play();
+}
+
+$().ready(function() {
+	//playAmbientSound();
+
+	$(ambientalAudio).bind("ended", function(){
+		playAmbientSound();
+	});
+
+	$('.backgroudTrigger').click(function() {
+		var newClass = $(this).attr('href');
+		$('body').removeClass().addClass(newClass);
+		return false;
+	});
+});
